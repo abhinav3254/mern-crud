8974920d58cf60e33d04dd545b704bc9b8ee1e3a
abhinav3254 <abhinav3254@gmail.com>
2024-01-12 22:11:28 +0530

    home done

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,8 +1,11 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Home.scss'
 import axios from 'axios';
 
 function Home() {
+
+    // State to store the API data
+    const [employees, setEmployees] = useState([]);
 
     useEffect(() => {
         getEmployees();
@@ -11,31 +14,42 @@ function Home() {
     // api call for home
     const getEmployees = () => {
         axios.get('http://localhost:8080')
-            .then(data => console.log(data.data))
+            .then(response => {
+                setEmployees(response.data);
+            })
             .catch(error => console.log(error));
     }
 
+    // Fetch data when the component mounts
+    useEffect(() => {
+        getEmployees();
+    }, []);
+
+
     return (
         <div>
-            <div className="card">
-                <div className="row">
-                    <p>John Doe</p>
-                    <p>john.doe@example.com</p>
+            {/* Map through the list of employees and render each employee card */}
+            {employees.map(employee => (
+                <div key={employee._id} className="card">
+                    <div className="row">
+                        <p>{employee.name}</p>
+                        <p>{employee.email}</p>
+                    </div>
+                    <div className="row">
+                        <p>{employee.address.street}</p>
+                        <p>{employee.address.city}</p>
+                    </div>
+                    <div className="row">
+                        <p>{employee.address.country}</p>
+                        <p>{employee.address.zip}</p>
+                    </div>
+                    <p>{employee.registerDate}</p>
+                    <div className="row">
+                        <button>Delete</button>
+                        <button>Update</button>
+                    </div>
                 </div>
-                <div className="row">
-                    <p>123 Main St</p>
-                    <p>Cityville</p>
-                </div>
-                <div className="row">
-                    <p>USA</p>
-                    <p>12345</p>
-                </div>
-                <p>2024-01-12T13:25:32.042+00:00</p>
-                <div className="row">
-                    <button>delete</button>
-                    <button>update</button>
-                </div>
-            </div>
+            ))}
         </div>
     )
 }
