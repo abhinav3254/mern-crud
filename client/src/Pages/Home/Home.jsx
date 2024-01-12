6f23333997907967c3ec66c0c31cf8772bcf4f31
abhinav3254 <abhinav3254@gmail.com>
2024-01-12 21:04:18 +0530

    api calling

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import './Home.scss'
+import axios from 'axios';
 
 function Home() {
+
+    useEffect(() => {
+        getEmployees();
+    })
+
+    // api call for home
+    const getEmployees = () => {
+        axios.get('http://localhost:8080')
+            .then(data => console.log(data.data))
+            .catch(error => console.log(error));
+    }
+
     return (
-        <div>Home</div>
+        <div>
+            <div className="card">
+                <div className="row">
+                    <p>John Doe</p>
+                    <p>john.doe@example.com</p>
+                </div>
+                <div className="row">
+                    <p>123 Main St</p>
+                    <p>Cityville</p>
+                </div>
+                <div className="row">
+                    <p>USA</p>
+                    <p>12345</p>
+                </div>
+                <p>2024-01-12T13:25:32.042+00:00</p>
+                <div className="row">
+                    <button>delete</button>
+                    <button>update</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
