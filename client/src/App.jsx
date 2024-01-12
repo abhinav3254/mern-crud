ed5c5c9a312275285102ad593bf79f2474c66150
abhinav3254 <abhinav3254@gmail.com>
2024-01-12 20:25:13 +0530

    client design

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Nav from './nav/Nav';
+
+function App() {
+  return (
+    <>
+      <Nav />
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/nav/Nav.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Nav.scss'
+
+function Nav() {
+    return (
+        <div className='main'>
+            <ul>
+                <li>Home</li>
+                <li>Add</li>
+                <li>Delete</li>
+                <li>Update</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Nav
