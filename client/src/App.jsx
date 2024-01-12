681479f04dea31182d4a63e11caaec005b07e8a8
abhinav3254 <abhinav3254@gmail.com>
2024-01-12 20:43:49 +0530

    routing and styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Pages/Home/Home';
+import Add from './Pages/Add/Add';
 import Nav from './nav/Nav';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Nav />
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/add' element={<Add />} />
+        </Routes>
+      </Router>
     </>
   );
 }
--- client/src/nav/Nav.jsx
+++ client/src/nav/Nav.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import './Nav.scss'
+import { NavLink } from 'react-router-dom'
+import logo from '../svg/logo.svg'
 
 function Nav() {
     return (
         <div className='main'>
-            <ul>
-                <li>Home</li>
-                <li>Add</li>
-                <li>Delete</li>
-                <li>Update</li>
-            </ul>
+            <div className="logo">
+                <img src={logo} alt="" />
+            </div>
+            <div className="links">
+                <ul>
+                    <li>
+                        <NavLink to='/'>Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/add'>Add</NavLink>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
